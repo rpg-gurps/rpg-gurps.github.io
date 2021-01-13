a2e7055c7a52965cb128920a9dd07d4fb69569c5
yscman <cvg.goncalves@protonmail.com>
2021-01-13 14:12:27 -0500

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'yscman'
+SITENAME = 'Epic Gurps'
+# SITEURL = 'rpg-gurps.github.io'
+SITETITLE = "JG Dungeon"
+SITESUBTITLE = "Bloody Adventures"
+SITEDESCRIPTION = "A place where luck and wits speak volumes"
+SITELOGO = 'images/logo_heros_rpg.jpeg'
+FAVICON = 'images/favicon.ico'
+# BROWSER_COLOR = "#333333"
+# PYGMENTS_STYLE = "monokai"
+DEFAULT_LANG = 'en'
+
+PATH = 'content'
+OUTPUT_PATH = "output"
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+THEME = "notmyidea"
+
+# ROBOTS = "index, follow"
+
+#PLUGIN_PATHS = ['./pelican-plugins']
+
+#PLUGINS = ['i18n_subsites']
+
+#JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+
+#I18N_TEMPLATES_LANG = "en"
+#DEFAULT_LANG = "en"
+#OG_LOCALE = "en_US"
+#LOCALE = "en_US"
+
+DATE_FORMATS = {
+    "en": "%B %d, %Y",
+}
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+USE_FOLDER_AS_CATEGORY = False
+MAIN_MENU = True
+HOME_HIDE_TAGS = True
+
+SOCIAL = (
+    ("github", "https://github.com/yscman"),
+    ("linkedin", "https://www.linkedin.com/in/cvgoncalves/"),
+    ("twitter", "https://twitter.com/cvg_goncalves"),
+    ("stack-overflow", "https://stackoverflow.com/users/13408448/vinny-cvg"),
+    ("envelope", "mailto:cvg.goncalves@protonmail.com"),
+)
+
+MENUITEMS = (
+    ("Archives", "/archives.html"),
+    ("Categories", "/categories.html"),
+    ("Tags", "/tags.html"),
+)
+
+
+DEFAULT_PAGINATION = 10
+
+STATIC_PATHS = ["images"]
+
+EXTRA_PATH_METADATA = {
+    "extra/ads.txt": {"path": "ads.txt"},
+    "extra/CNAME": {"path": "CNAME"},
+}
+
+THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+THEME_COLOR_ENABLE_USER_OVERRIDE = True
+
+USE_LESS = True
+
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
